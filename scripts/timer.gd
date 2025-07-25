7c06bd611ba8d85f7474d25a132c6203eec81d8b
Uncertawn <happyjapanesesan@gmail.com>
2025-07-25 06:45:47 +0200

    ability to loose

--- scripts/timer.gd
+++ scripts/timer.gd
@@ -1,7 +1,12 @@
 extends Label
 
 func _ready() -> void:
-	$Timer.start(600)
+	#$Timer.start(600)
+	$Timer.start(300)
 
 func _process(delta: float) -> void:
 	text = "Time: " + str(int($Timer.time_left))
+
+
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
